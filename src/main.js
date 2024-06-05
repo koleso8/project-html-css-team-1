3a89d455b2b663621f45caf322ebc91343994f56
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-06-05 13:35:29 +0100

    add-smooth-scroll-to-anchor

--- src/main.js
+++ src/main.js
@@ -20,3 +20,39 @@ document.addEventListener('click', event => {
   }
 });
 //
+
+// Smooth scroll to anchor
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetElement = document.querySelector(this.getAttribute('href'));
+
+    if (targetElement) {
+      const targetPosition =
+        targetElement.getBoundingClientRect().top + window.pageYOffset;
+      const startPosition = window.pageYOffset;
+      const distance = targetPosition - startPosition;
+      const duration = 500;
+      let startTime = null;
+
+      function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+      }
+
+      function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return (c / 2) * t * t + b;
+        t--;
+        return (-c / 2) * (t * (t - 2) - 1) + b;
+      }
+
+      requestAnimationFrame(animation);
+    }
+  });
+});
+//
