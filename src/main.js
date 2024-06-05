74449e9d0b20ccb3e0894bc9586a46c55b772ee8
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-06-05 13:16:43 +0100

    add-sections-animation

--- src/main.js
+++ src/main.js
@@ -20,3 +20,24 @@ document.addEventListener('click', event => {
   }
 });
 //
+
+// Sections animation
+let blocks = document.querySelectorAll('.section-animation');
+
+function checkBlocksVisibility() {
+  let windowHeight = window.innerHeight;
+
+  blocks.forEach(block => {
+    let blockPosition = block.getBoundingClientRect().top;
+
+    if (blockPosition < windowHeight - 100) {
+      block.style.opacity = '1';
+      block.style.transform = 'translateY(0)';
+    }
+  });
+}
+
+checkBlocksVisibility();
+
+window.addEventListener('scroll', checkBlocksVisibility);
+//
